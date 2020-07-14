983ebfb5793396a122927ea560963b609475cfcc
Victor Pereira <victor_p_97_@hotmail.com>
2020-07-14 17:33:30 -0300

    Jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -130,6 +130,19 @@ namespace xadrez {
                 throw new TabuleiroException("Você não pode se colocar em Xeque!");
             }
 
+            Peca p = tab.peca(destino);
+            
+            // #jogadaespecial promocao
+            if(p is Peao) {
+                if((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7)) {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual))) {
                 xeque = true;
             } 
@@ -144,7 +157,7 @@ namespace xadrez {
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             //#jogadaespecial En Passant
             if(p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2)) {
